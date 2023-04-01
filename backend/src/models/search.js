1a438d7eee7fed95465b6b71b7ffd18c4973251a
VIKRAM SINGH <67967129+vikky413@users.noreply.github.com>
2023-04-01 13:29:05 +0530

    Add files via upload

---
+++ backend/src/models/search.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const searchSchema = new Schema({
+     ftrain : {
+        type:String,
+        required:true,
+    },
+    ttrain : {
+        type:String,
+        required:true,
+    },
+});
+
+// Compile model from schema
+const searchModel = mongoose.model("searchModel", searchSchema);
+module.exports = searchModel
